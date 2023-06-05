0781b75093af0a38c1b48054b9253a53962740df
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-06-06 01:30:22 +0200

    very-basic variables

--- Learning-solidity/basics/basic-variables.sol
+++ Learning-solidity/basics/basic-variables.sol
@@ -4,7 +4,7 @@ contract SimpleStorage{
 
 //basic variable types
 
-    uint256 favouriteNum = 5;
+    uint256 favouriteNum = 5;  //if I wrote uint256 favouriteNum; it getrs intialized to the null value (0)
     bool favouriteBool = true;
     string favString = "Ali";
     address favAddress = 0x2B148F2E8205838Dd0c1BB9B3F9E313d7F350525;
