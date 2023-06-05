9d2fa330dff20750bb52ec972452c640d84e8be5
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-06-06 01:28:39 +0200

    very-basic variable types

---
+++ Learning-solidity/basics/basic-variables.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.6.0 < 0.9.0;
+
+contract SimpleStorage{
+
+//basic variable types
+
+    uint256 favouriteNum = 5;
+    bool favouriteBool = true;
+    string favString = "Ali";
+    address favAddress = 0x2B148F2E8205838Dd0c1BB9B3F9E313d7F350525;
+    bytes32 favByte = "cats are cool";
+}
