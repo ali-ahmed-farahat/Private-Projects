510365de3f078cd087fcda649c3eeb747203b934
ali-ahmed-farahat <70855581+ali-ahmed-farahat@users.noreply.github.com>
2023-06-09 00:23:39 +0200

    Registration-Related functions

--- Python/Cinema-Project/Register.py
+++ Python/Cinema-Project/Register.py
@@ -1,20 +1,31 @@
-import json
+import os.path
+
+def createFile(pathfile):
+    if not os.path.exists(pathfile):
+        open(pathfile,'a+')
+        return True
+    
 def register(username,password,pathfile):
     try:
-        with open(pathfile,'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                
-                fields = line.split(",")
-                if registercheck(username,pathfile):
-                    print("This user already exists")
-                    return False
-                else:
-                    NewUser = (username + ',' + password)
-                    file = open(pathfile,'a')
-                    file.write('\n' + NewUser + '\nblank')
-                    print("Registered Successfully")
-                    return True
+        if createFile(pathfile):
+            f=open(pathfile,'a+')
+            NewUser = (username + ',' + password)    
+            f.write(NewUser)
+            print("Registered Successfully")
+            return
+        else:
+            with open(pathfile,'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if registercheck(username,pathfile):
+                        print("This user already exists")
+                        return False
+                    else:
+                        file = open(pathfile,'a')
+                        NewUser = (username + ',' + password)    
+                        file.write('\n' + NewUser)
+                        print("Registered Successfully")
+                        return True
     except Exception:
         print(Exception)
 def registercheck(username,pathfile):
@@ -30,8 +41,3 @@ def registercheck(username,pathfile):
     except Exception:
         print(Exception)
     return False
-
-
-
-
-
